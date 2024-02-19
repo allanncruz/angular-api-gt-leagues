60f2869e9090ce6a6620068291434e152f2b5b93
allan <allan.dantas@redspark.io>
2024-02-19 14:31:17 -0300

    feat: signin email and passowrd

--- src/app/root/auth.service.ts
+++ src/app/root/auth.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  user: any;
+  error: any;
+
+  constructor(public auth: AngularFireAuth) {}
+
+  async emailSignin(email: string, password: string) {
+    try {
+      const credential = await this.auth.
+      signInWithEmailAndPassword(
+        email, 
+        password
+      );
+      this.user = credential.user;
+
+    } catch (error) {
+      this.error = error;
+    }  
+  }
 }
