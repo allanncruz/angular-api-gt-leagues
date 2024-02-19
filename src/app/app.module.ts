8b26c9e24015a5c7045d7ad25d7cd447aa2e6062
allan <allan.dantas@redspark.io>
2024-02-19 14:18:16 -0300

    feat: config firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AuthService } from './root/auth.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +15,21 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyASnn7KUCLuPvrS_Jboyk0If-mRZABCMDM",
+      authDomain: "angular-auth-df76e.firebaseapp.com",
+      databaseURL: "https://angular-auth-df76e-default-rtdb.firebaseio.com",
+      projectId: "angular-auth-df76e",
+      storageBucket: "angular-auth-df76e.appspot.com",
+      messagingSenderId: "644096836227",
+      appId: "1:644096836227:web:47bad91a59fe97ec0251b6"
+    }),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
