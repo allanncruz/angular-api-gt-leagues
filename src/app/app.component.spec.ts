b871b7c59b0e95930a3cc9a80b9bf5b88591a62d
allan <allan.dantas@redspark.io>
2024-02-19 13:55:06 -0300

    reafctor: downgrade to v15

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,10 +3,16 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-auth'`, () => {
+  it(`should have as title 'ProjectAngular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-auth');
+    expect(app.title).toEqual('ProjectAngular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-auth app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ProjectAngular app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CpNavbarComponent } from './cp-navbar/cp-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CpNavbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
