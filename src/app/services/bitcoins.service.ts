2e05f872169f19d224233b8c9c333248566ffd19
allan <allan.dantas@redspark.io>
2024-02-20 16:57:07 -0300

    feat: gt league api request

--- src/app/services/bitcoins.service.ts
+++ src/app/services/bitcoins.service.ts
@@ -1,46 +1,47 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-interface ResponseCoins {
-  time: {
-    updated: string
+interface Response extends Array<Response> {
+  updatedAt: string,
+  result: {
+    stats: {
+      away_score: number,
+      home_score: number
+    }
   },
-  bpi: {
-    USD: {
-      symbol: string,
-      rate_float: number
-    };
-    BRL: {
-      symbol: string,
-      rate_float: number
-    };
-  };
+  participants: Participants  
 }
+
+interface Participants extends Array<Participants> {
+  participant: {
+    team :{
+      crest: string,
+      name: string
+    }
+    player :{
+      nickname: string
+    }
+  }
+};
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class BitcoinsService {
-  currentPrice = {} as ResponseCoins;
-  history = [] as Array<ResponseCoins>;
+  listResults = {} as Response;
 
   constructor(private http: HttpClient) {
     this.update();
   }
 
   update() {
-    let url = 'https://api.coindesk.com/v1/bpi/currentprice/BRL.json';
-    this.http.get<ResponseCoins>(url).subscribe(data => {
-      if(this.currentPrice.bpi){
-        let current = this.currentPrice.bpi.USD.rate_float;
-        let readed = data.bpi.USD.rate_float 
-        if (current && current != readed) {
-          this.history.push(data);
-        }
-      } else {
-        this.history.push(data);
-      }
-      this.currentPrice = data;
+    let url = 'https://hr.gtleagues.com/api/fixtures?kickoff=between%3A2023-11-04T03%3A00%3A00.000Z%2C2023-11-05T02%3A59%3A59.999Z&limit=50&offset=0&sort=-kickoff%2C-matchNr&status=in%3A3%2C5%2C4%2C6&xtc=true';
+    this.http.get<Response>(url).subscribe(data => {
+
+      this.listResults = data;
+
+      console.log(data)
     })
   }
 }
