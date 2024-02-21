03a7fc275108d43aaf94168fac1481bda7124096
allan <allan.dantas@redspark.io>
2024-02-21 18:41:21 -0300

    feat: update api

--- src/app/services/apis.service.ts
+++ src/app/services/apis.service.ts
@@ -36,7 +36,7 @@ export class GTLeagueService {
   }
 
   update() {
-    let gtLeagueUrl = 'https://hr.gtleagues.com/api/fixtures?kickoff=between%3A2023-11-04T03%3A00%3A00.000Z%2C2023-11-05T02%3A59%3A59.999Z&limit=50&offset=0&sort=-kickoff%2C-matchNr&status=in%3A3%2C5%2C4%2C6&xtc=true';
+    let gtLeagueUrl = 'https://api.gtleagues.com/api/fixtures?kickoff=between%3A2024-02-21T03%3A00%3A00.000Z%2C2024-02-22T02%3A59%3A59.999Z&limit=50&offset=0&sort=-kickoff%2C-matchNr&status=in%3A3%2C5%2C4%2C6&xtc=true';
     this.http.get<GtLeagueResponse>(gtLeagueUrl).subscribe(data => {
       this.listResults = data;
     })
