e0ce1786bcec66ada2bfa6a8689d7f993c76b71c
Allan <contato@allancruz.com.br>
2024-02-29 07:36:47 -0300

    feat: now and tomorrow date

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -6,33 +6,40 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  today: Date = new Date();
+  currentDate = '' as string;
+  tomorrowDate = '' as string;
   apiGtLeague: string = "https://api.gtleagues.com/api/";
   limit: number = 1000;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.setCurrentDate();
+  }
+
+  setCurrentDate() {
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
+
+    this.currentDate = this.formatDate(today);
+    this.tomorrowDate = this.formatDate(tomorrow);
+  }
+
+  formatDate(date: Date): string {
+    const year = date.getFullYear();
+    const month = this.padZero(date.getMonth() + 1);
+    const day = this.padZero(date.getDate());
+    return `${year}-${month}-${day}`;
+  }
+
+  padZero(num: number): string {
+    return num < 10 ? `0${num}` : `${num}`;
+  }
 
   fetchSeasonsData(): Observable<any> {
     return this.http.get(`${this.apiGtLeague}seasons?limit=${this.limit}&offset=0&status=1`);
   }
 
   fetchResultsData(): Observable<any> {
-    const year  = this.today.getFullYear();
-    const month = this.today.getMonth() + 1;
-    const day   = this.today.getDate(); 
-    const dayTo = this.today.getDate() + 1; 
-
-    let monthValue = '';
-    let dayValue   = '';
-    let dayValueTo = '';
-
-    monthValue = ((month < 10) ? '0' : '').concat(month.toString())
-    dayValue   = ((day < 10) ? '0' : '').concat(day.toString())
-    dayValueTo = ((dayTo < 10) ? '0' : '').concat(dayTo.toString())
-
-    const kickoffFrom = year.toString().concat('-').concat(monthValue).concat('-').concat(dayValue);
-    const kickoffTo   = year.toString().concat('-').concat(monthValue).concat('-').concat(dayValueTo);
-    
-    return this.http.get(`${this.apiGtLeague}fixtures?kickoff=between%3A${kickoffFrom}T03%3A00%3A00.000Z%2C${kickoffTo}T02%3A59%3A59.999Z&limit=50&offset=0&sort=-kickoff%2C-matchNr&status=in%3A3%2C5%2C4%2C6&xtc=true`);
+    return this.http.get(`${this.apiGtLeague}fixtures?kickoff=between%3A${this.currentDate}T03%3A00%3A00.000Z%2C${this.tomorrowDate}T02%3A59%3A59.999Z&limit=50&offset=0&sort=-kickoff%2C-matchNr&status=in%3A3%2C5%2C4%2C6&xtc=true`);
   }
 }
